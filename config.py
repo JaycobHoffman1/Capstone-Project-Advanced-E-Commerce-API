747c43324cc335152cb9cca8d26db89c35d741a5
JaycobHoffman1 <jaycobhoffman2004@gmail.com>
2025-02-28 22:07:07 -0800

    final commit fix

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class DevelopmentConfig:
     SQLALCHEMY_DATABASE_URI = 'postgresql://advanced_e_commerce_db_postgres_user:jxD0opwvx3vr76MNCIDr4ctraNEREdzp@dpg-cv19s3lds78s73dkugc0-a.oregon-postgres.render.com/advanced_e_commerce_db_postgres'
-    SQLALCHEMY_TRACK_MODIFICATIONS = Falsegit 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     CACHE_TYPE = 'SimpleCache'
     DEBUG = True
